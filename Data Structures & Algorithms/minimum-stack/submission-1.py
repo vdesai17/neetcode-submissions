db3b2e2ec3df00142af6b6995498b5efd91d4c3a
Vivan Desai <vivandesai17@outlook.com>
2026-09-02 19:47:34 -0400

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,21 @@
+class MinStack:
+
+    def __init__(self):
+        # Stores tuples: (value, min_at_this_point)
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.stack.append((val, val))
+        else:
+            current_min = min(val, self.stack[-1][1])
+            self.stack.append((val, current_min))
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
